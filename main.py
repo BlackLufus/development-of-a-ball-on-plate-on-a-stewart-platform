02469bdb594e806e1df2c6aea508f2bbc0af445f
BlackLufus <michi-reischl@web.de>
2025-03-14 14:16:15 +0100

    Fix servo compatibility, that the servo are able to rotate between an angle of 0 and 135 degrees

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ from StewartPlattform import StewartPlattform
 # Standard deviation for each angle so that the r-axis is horizontal
 # servo: angle deviation
 deviation = {
-    0: 7,  # Rechts
-    1: 6,  # Links
-    2: 4,  # Rechts
-    3: 3,  # Links
-    4: -7, # Rechts
-    5: 0   # Links
+    0: 0,  # Rechts
+    1: 10,  # Links
+    2: -30,  # Rechts
+    3: 0,  # Links
+    4: 10, # Rechts
+    5: -50   # Links
 }
 
 # Set the rotation angle for the Steward Plattform (between: -45 and 45)
 def setRotationAngle(servo, angle):
-    if angle >= -90 and angle <= 90 and servo in deviation:
-        temp = (angle if servo % 2 == 0 else -angle)
-        temp += (deviation[servo] if servo % 2 == 0 else -deviation[servo]) # Add angle deviation
-        temp += 90 # Transform to normal angle
-        pwm.setRotationAngle(servo, temp)
+    if angle >= 0 and angle <= 135 and servo in deviation:
+        minAngle = 921
+        maxAngle = 1780
+        temp = 1780 - 921
+        temp /= 90
+        temp *= angle
+        if servo % 2 == 0:
+            pwm.setServoPulse(servo, minAngle + temp + deviation[servo])
+        else:
+            pwm.setServoPulse(servo, maxAngle - temp - deviation[servo])
     else:
         print("Angle out of range")
 
@@ -31,7 +36,7 @@ def servoTest():
     while True:
         print("Move Up!")
 
-        for angle in range(-45, 45, 1):
+        for angle in range(0, 90, 1):
             if angle == 0:
                 print("zero point")
             for channel in range(6):
@@ -40,7 +45,7 @@ def servoTest():
 
         print("Move Down!")
         
-        for angle in range(45, -45, -1):
+        for angle in range(90, 0, -1):
             if angle == 0:
                 print("zero point")
             for channel in range(6):
@@ -56,13 +61,13 @@ def differentAngleServoTest(angleDict):
         setRotationAngle(channel, angleDict[channel])
 
 def angleServoTest():
-    stewart = StewartPlattform(100, [340, 20, 100, 140, 240, 280], 80, [350, 10, 110, 130, 250, 270], 40, 100)
-    leg_length_list = stewart.calculate(0, 0, 110, 25, 0, 0)
+    stewart = StewartPlattform(100, [340, 20, 100, 140, 240, 280], 80, [350, 10, 110, 130, 250, 270])
+    leg_length_list = stewart.calculate(0, 0, 88, 25, 0, 0)
     if leg_length_list != None:
         for index, leg_length in enumerate(leg_length_list):
             print("length of leg ", index, ": ", leg_length)
 
-    angle_list = stewart.getAngles(leg_length_list)
+    angle_list = stewart.getAngles(40, 100, leg_length_list)
     for index, angle in enumerate(angle_list):
         print("angle of servo ", index, ": ", angle)
         setRotationAngle(index, angle)
@@ -73,30 +78,28 @@ if __name__ == '__main__':
     pwm = PCA9685()
     pwm.setPWMFreq(50)
 
-    # Debugging: Ausgabe der base_points und plattform_points einzeln
-    # print("Base Points:", swp.base_points)
-    # print("Plattform Points:", swp.plattform_points)
+    angleServoTest()
 
     # differentAngleServoTest({
     #     0: 24,
-    #     1: -38,
+    #     1: 83,
     #     2: 9,
-    #     3: 39,
-    #     4: -5,
-    #     5: -45
+    #     3: 84,
+    #     4: 5,
+    #     5: 90
     # })
 
     # time.sleep(1)
 
-    # quickServoTest(20)
+    # quickServoTest(80)
 
     # time.sleep(1)
 
-    # quickServoTest(45)
+    # quickServoTest(55)
 
     # time.sleep(1)
 
-    # quickServoTest(-10)
+    # quickServoTest(10)
 
     # time.sleep(1)
 
