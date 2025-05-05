a3b2bc4517d405df3238e947258f8f9a7cffca06
blacklufus <michael.reischl@studmail.htw-aalen.de>
2025-05-05 17:13:04 +0200

    Add alt video capture via PyAv library.

--- video_cam.py
+++ video_cam.py
@@ -1,59 +1,103 @@
 # https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
 import asyncio
 import time
+import av
 import numpy as np
 import cv2
 import datetime
+import threading
+
+class CameraThreadWithAV:
+    def __init__(
+            self, 
+            device_name = "Logitech BRIO", 
+            options = {
+                "video_size": "1280x720",
+                "framerate": "60"
+            },
+            # options = {
+            #     "video_size": "1920x1080",
+            #     "framerate": "30"
+            # }
+        ):
+        self.frame_num = 0
+        self.container = av.open(f"video={device_name}", options=options, format="dshow")
+        self.img = None
+        
+        self.running = True
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        fps_times = []
+        for frame in self.container.decode(video=0):
+            if not self.running:
+                break
+            self.img = frame.to_ndarray(format="bgr24")
+            self.frame_num += 1
+
+            # Calculate fps
+            now = time.time()
+            fps_times.append(now)
+            fps_times = [t for t in fps_times if now - t < 1]
+            self.fps = len(fps_times)
+    
+    def read(self):
+        if self.img is None:
+            return None, 0, 0
+        return self.img.copy(), self.fps, self.frame_num
+    
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        self.container.close()
+    
+class CameraThread:
+    def __init__(self, cam_index=0):
+        self.frame_num = 0
+
+        self.cap = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
+        self.ret, self.frame = self.cap.read()
+        self.fps = 1
+
+        self.running = True
+        self.thread = threading.Thread(target=self.update, daemon=True)
+        self.thread.start()
+
+    def update(self):
+        fps_times = []
+        while self.running:
+            self.ret, self.frame = self.cap.read()
+            self.frame_num += 1
+
+            # Calculate fps
+            now = time.time()
+            fps_times.append(now)
+            fps_times = [t for t in fps_times if now - t < 1]
+            self.fps = len(fps_times)
+
+    def read(self):
+        return self.ret, self.frame, self.fps, self.frame_num
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+        self.cap.release()
 
 class VideoCam:
-
-    def __init__(self):
+    def __init__(self, video_source=0):
         # Open the default camera
-        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.cap = cv2.VideoCapture(video_source, cv2.CAP_DSHOW)
     
-    async def start(self):
-
-        # Get the default fps, frame width and height
-        frame_fps = int(self.cap.get(cv2.CAP_PROP_FPS))
-        frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter('output.mp4', fourcc, frame_fps, (frame_width, frame_height))
-
-        if not self.cap.isOpened():
-            print("Error: Unable to open the video source.")
-            return
-
+    def update(self):
         while self.cap.isOpened():
-            ret, frame = self.cap.read()
-            if not ret:
-                print("Video source ended or error while reading.")
-                break
+            self.ret, self.frame = self.cap.read()
 
-            # Write the frame to the output file
-            out.write(frame)
-
-            # Display the captured frame
-            cv2.imshow('Camera', frame)
-
-            # Press 'q' to exit the loop
-            if cv2.waitKey(1) == ord('q'):
-                break
-        
-        # Release the capture and writer objects
-        self.cap.release()
-        out.release()
-        cv2.destroyAllWindows()
+    def read(self):
+        return self.ret, self.frame
     
-    def get_frame(self):
-        return self.cap.read()
-
-# This is an async test function to check video cam is working proper
-def get_frame_as_stream():
-    cam = VideoCam()
-    asyncio.run(cam.start())
+    def stop(self):
+        self.cap.release()
 
 # A Function to calculate frame per seconds
 def get_fps(array: np.array):
@@ -61,33 +105,98 @@ def get_fps(array: np.array):
     array = np.append(array, dt)
     match = (dt - datetime.timedelta(seconds=1))
     array = array[array >= match]
-    return array, len(array)
+    return array, len(array), round(float(array[-1].timestamp() - array[-2].timestamp()), 5) if len(array) >= 2 else 0
 
 # This is a test function to get frame by frame
-def get_frame_with_delay(fps: int):
-    time_stamp_list = np.array([],dtype=np.int64)
+def get_frame_with_delay(debug = True):
+    time_stamp_list = np.array([],dtype=np.float32)
+    fps = 20
     delay: float = 1 / fps
-    cam = VideoCam()
-    last_time = time.time()
+    cam = VideoCam(video_source=2)
+    cam.cap.set(cv2.CAP_PROP_FPS, fps)
     while cam.cap.isOpened():
-        current_time = time.time()
-        delta_time = current_time - last_time
-        if delta_time >= delay:
-            last_time = current_time
-            ret, frame = cam.get_frame()
-            if not ret:
-                print("Videoquelle beendet oder Fehler beim Lesen.")
-            else:
-                # Display the captured frame
-                cv2.imshow('Camera', frame)
+        ret, frame = cam.read()
+        if not ret:
+            print("Videoquelle beendet oder Fehler beim Lesen.")
+        else:
+            # Display the captured frame
+            cv2.imshow('Camera', frame)
 
-                # Press 'q' to exit the loop
-                if cv2.waitKey(1) == ord('q'):
-                    cv2.destroyAllWindows()
-            # time.sleep(delay)
-            time_stamp_list, fps = get_fps(time_stamp_list)
-            print(f"FPS {fps}")
+            # Press 'q' to exit the loop
+            if cv2.waitKey(1) == ord('q'):
+                cv2.destroyAllWindows()
+        time.sleep(delay)
+        if debug:
+            time_stamp_list, fps, length = get_fps(time_stamp_list)
+            print(f"FPS {fps} ({length})")
+
+# Get available sources
+def returnCameraIndexes():
+    arr = []
+    for index in range(5):
+        cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+    return arr
+
+def get_frame_with_thread():
+    cam = CameraThread(cam_index=0)
+    
+    last_frame_num = cam.frame_num
+    try:
+        while True:
+            
+            if last_frame_num != cam.frame_num:
+                last_frame_num = cam.frame_num
+            
+                ret, frame, fps, frame_num = cam.read()
+                if not ret:
+                    print("Fehler beim Lesen des Frames")
+                    break
+
+                print(f"FPS: {fps} (num: {frame_num})")
+
+                # Anzeige des Bildes
+                cv2.imshow("Threaded Camera", frame)
+
+                # Mit 'q' beenden
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+    finally:
+        cam.stop()
+        cv2.destroyAllWindows()
+
+def get_frame_with_av_thread():
+    cam = CameraThreadWithAV()
+
+    last_frame_num = cam.frame_num
+    try:
+        while True:
+            if last_frame_num != cam.frame_num:
+                last_frame_num = cam.frame_num
+
+            frame, fps, frame_num = cam.read()
+            if frame is not None:
+                print(f"FPS: {fps} (num: {frame_num})")
+
+                # Anzeige des Bildes
+                cv2.imshow("Threaded Camera", frame)
+
+                # Mit 'q' beenden
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+    finally:
+        cam.stop()
+            
+
 
 if __name__ == "__main__":
-    # get_frame_as_stream()
-    get_frame_with_delay(20)
+    get_frame_with_av_thread()
+    # get_frame_with_thread()
+    # get_frame_as_stream(video_source=0)
+    # print(returnCameraIndexes())
+    # get_frame_with_delay()
