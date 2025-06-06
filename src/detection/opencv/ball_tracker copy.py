cdc1c0144fa32752a4bd3c8f8de9bb4984ffdbf4
BlackLufus <michi-reischl@web.de>
2025-06-06 13:57:09 +0200

    Fix an issue where image was to bright and detection software was not able to recognize it correctly.

--- src/detection/opencv/ball_tracker copy.py
+++ src/detection/opencv/ball_tracker copy.py
@@ -3,8 +3,7 @@ import time
 import numpy as np
 
 class BallTracker:
-    def __init__(self, camera_index=0, frame_width=800, frame_height=600, debug=False):
-        self.debug = debug
+    def __init__(self, camera_index=0, frame_width=800, frame_height=600):
         self.cap = cv2.VideoCapture(camera_index, cv2.CAP_V4L2)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
@@ -22,21 +21,17 @@ class BallTracker:
 
         # Convert image from rgb to gray
         img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-        if self.debug:
-            cv2.imshow('img_gray', img_gray)
+        cv2.imshow('img_gray', img_gray)
 
         # Get binarty image
-        _, tresh = cv2.threshold(img_gray, 130, 255, cv2.THRESH_BINARY)
-        if self.debug:
-            cv2.imshow('tresh', tresh)
+        _, tresh = cv2.threshold(img_gray, 170, 255, cv2.THRESH_BINARY)
+        cv2.imshow('tresh', tresh)
 
         # Get all contours
         contours, _ = cv2.findContours(tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # To prevent manipulating the original image we create a copy of it
         img_rgb_copy = img_rgb.copy()
-
-        # contours = [cnt for cnt in contours if self.check_circle(cv2.minEnclosingCircle(cnt)[1], cv2.contourArea(cnt))]
 
         # Check if any contour exist
         if contours:
@@ -60,20 +55,11 @@ class BallTracker:
             cv2.ellipse(img_rgb_copy, ellipse, (0, 255, 0), 4)
 
 
-            if self.debug:
-                cv2.imshow("Detected Circle", img_rgb_copy)
+            cv2.imshow("Detected Circle", img_rgb_copy)
             # Return features
             return center
         else:
             return None
-    
-    def check_circle(self, radius, area):
-        circle_area = np.pi * (radius ** 2)
-
-        ratio = area / circle_area
-        if 0.6 < ratio < 1.2:
-            return True
-        return False
 
     def release_camera(self):
         # Kamera freigeben
@@ -81,7 +67,7 @@ class BallTracker:
 
 # Beispielverwendung
 if __name__ == "__main__":
-    tracker = BallTracker(camera_index=0, debug=True)
+    tracker = BallTracker(camera_index=0)
 
     while True:
         ball_position = tracker.get_ball_position()
